048317555028877e12a349b8c73e25889bdcdc10
sandesh <qalamsal8@gmail.com>
2020-07-23 12:01:00 -0500

    master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world ! from github.com..");
-        System.out.print("Sandes Lamsal");
+        System.out.print("Sandes Lamsal ..");
         //System.out.println("Java Automation\n");
 
         /*
